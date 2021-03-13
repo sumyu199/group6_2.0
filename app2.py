5627fe59f3f34eb4c1f8e749b41552522bf3b530
sumyu199 <69990961+sumyu199@users.noreply.github.com>
2021-03-13 20:22:12 +0000

    not all

--- app2.py
+++ app2.py
@@ -612,22 +612,23 @@ for i in indicators:
 st.header('Technical Analysis Indications')
 tech_df = tech_df.dropna(thresh=2)
 tech_df.index = range(len(tech_df))
-if 'BB_Buy_Signal_Price' in tech_df.columns:
+singal = ['BB_Buy_Signal_Price','MACD_Buy_Signal_Price','BB_Buy_Signal_Price']
+if 'BB_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['BB_Buy_Signal_Price']).applymap(red, subset=['BB_Sell_Signal_Price']))
-if 'MACD_Buy_Signal_Price' in tech_df.columns:
+if 'MACD_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['MACD_Buy_Signal_Price']).applymap(red, subset=['MACD_Sell_Signal_Price']))
-if 'OBV_Buy_Signal_Price' in tech_df.columns:
+if 'OBV_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['OBV_Buy_Signal_Price']).applymap(red, subset=['OBV_Sell_Signal_Price']))
-if 'BB_Buy_Signal_Price' and 'MACD_Buy_Signal_Price' in tech_df.columns:
+if 'BB_Buy_Signal_Price' and 'MACD_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['BB_Buy_Signal_Price']).applymap(red, subset=['BB_Sell_Signal_Price'])
         .applymap(green, subset=['MACD_Buy_Signal_Price']).applymap(red, subset=['MACD_Sell_Signal_Price']))
-if 'BB_Buy_Signal_Price' and 'OBV_Buy_Signal_Price' in tech_df.columns:
+if 'BB_Buy_Signal_Price' and 'OBV_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['BB_Buy_Signal_Price']).applymap(red, subset=['BB_Sell_Signal_Price']).
                  applymap(green, subset=['OBV_Buy_Signal_Price']).applymap(red, subset=['OBV_Sell_Signal_Price']))
-if 'MACD_Buy_Signal_Price' and 'OBV_Buy_Signal_Price' in tech_df.columns:
+if 'MACD_Buy_Signal_Price' and 'OBV_Buy_Signal_Price' in tech_df.columns and not all(singal) in tech_df.columns:
     st.dataframe(tech_df.style.applymap(green, subset=['MACD_Buy_Signal_Price']).applymap(red, subset=['MACD_Sell_Signal_Price']).
                   applymap(green, subset=['OBV_Buy_Signal_Price']).applymap(red, subset=['OBV_Sell_Signal_Price']))
-if 'BB_Buy_Signal_Price' and 'MACD_Buy_Signal_Price' and 'OBV_Buy_Signal_Price' in tech_df.columns:
+if all(singal) in tech_df.columns:
      st.dataframe(tech_df.style.applymap(green, subset=['BB_Buy_Signal_Price']).applymap(red, subset=['BB_Sell_Signal_Price'])
                   .applymap(green, subset=['MACD_Buy_Signal_Price']).applymap(red, subset=['MACD_Sell_Signal_Price']).
                   applymap(green, subset=['OBV_Buy_Signal_Price']).applymap(red, subset=['OBV_Sell_Signal_Price']))
